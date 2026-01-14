6e6ce3b455420b61aad70a18cbd1311e0395a578
li wt <lwt20010908@gmail.com>
2026-01-14 08:29:22 +0000

    File changes

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -389,7 +389,7 @@ export default function Marketplace() {
                         </DialogFooter>
                     </DialogContent>
                 </Dialog>
-                </div>
-                </div>
-                );
-                }
+            </div>
+        </div>
+    );
+}
