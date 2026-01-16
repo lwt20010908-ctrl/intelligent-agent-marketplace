e3c79e71742b51699617177215baf48620d559fc
li wt <lwt20010908@gmail.com>
2026-01-16 09:26:50 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,31 +1,31 @@
-import About from './pages/About';
 import AgentDetail from './pages/AgentDetail';
 import AgentManagement from './pages/AgentManagement';
 import CaseStudies from './pages/CaseStudies';
+import Dashboard from './pages/Dashboard';
 import Developer from './pages/Developer';
-import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import MerchantWorkspace from './pages/MerchantWorkspace';
 import Resources from './pages/Resources';
 import TalentShowcase from './pages/TalentShowcase';
 import WorkspaceAgents from './pages/WorkspaceAgents';
-import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import About from './pages/About';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "About": About,
     "AgentDetail": AgentDetail,
     "AgentManagement": AgentManagement,
     "CaseStudies": CaseStudies,
+    "Dashboard": Dashboard,
     "Developer": Developer,
-    "Home": Home,
     "Marketplace": Marketplace,
     "MerchantWorkspace": MerchantWorkspace,
     "Resources": Resources,
     "TalentShowcase": TalentShowcase,
     "WorkspaceAgents": WorkspaceAgents,
-    "Dashboard": Dashboard,
+    "Home": Home,
+    "About": About,
 }
 
 export const pagesConfig = {
